eaa161b2e1aacb58b8670abc0f8acf2224340669
Themis Makedas <themis.makedas@weare8.com>
2023-11-14 23:48:48 +0200

    Added ApplicationLayer package

--- ApplicationLayer/Sources/ApplicationLayer/User.swift
+++ ApplicationLayer/Sources/ApplicationLayer/User.swift
@@ -1,8 +1,14 @@
 //
-//  File.swift
-//  
+//  User.swift
+//
 //
 //  Created by Themis Makedas on 14/11/23.
 //
 
 import Foundation
+
+struct User {
+    let id: String
+    let name: String
+    let username: String
+}
