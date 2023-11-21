3960abb435b34ffa5a1babb33e53c9624dcdeed5
Themis Makedas <themis.makedas@weare8.com>
2023-11-21 20:29:06 +0200

    Initialization

--- SampleApp/SampleApp/ContentView.swift
+++ SampleApp/SampleApp/ContentView.swift
@@ -5,4 +5,21 @@
 //  Created by Themis Makedas on 21/11/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello world!")
+    }
+}
+
+#if DEBUG
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+#endif
--- SampleApp/SampleApp/SampleApp.swift
+++ SampleApp/SampleApp/SampleApp.swift
@@ -5,4 +5,16 @@
 //  Created by Themis Makedas on 21/11/23.
 //
 
-import Foundation
+//import Foundation
+//import SwiftUI
+//
+//@main
+//struct SampleApp: App {
+//  @UIApplicationDelegateAdaptor var delegate: AppDelegate
+//
+//  var body: some Scene {
+//    WindowGroup {
+//      ContentView()
+//    }
+//  }
+//}
