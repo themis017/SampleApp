97bf51cae850d775cd9c93199cf672567ca6a274
Themis Makedas <themis.makedas@weare8.com>
2023-11-24 19:49:42 +0200

    Refactor

--- SampleApp/SampleApp/SampleApp.swift
+++ SampleApp/SampleApp/SampleApp.swift
@@ -5,16 +5,20 @@
 //  Created by Themis Makedas on 21/11/23.
 //
 
-//import Foundation
-//import SwiftUI
-//
-//@main
-//struct SampleApp: App {
-//  @UIApplicationDelegateAdaptor var delegate: AppDelegate
-//
-//  var body: some Scene {
-//    WindowGroup {
-//      ContentView()
-//    }
-//  }
-//}
+import Foundation
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    
+//    @UIApplicationDelegateAdaptor
+//    var delegate: AppDelegate
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
