22e9d61a023ba8bd0ab2360c82650d59968443d1
Themis Makedas <themis.makedas@weare8.com>
2024-01-18 22:15:25 +0200

    Added TabBarRoutes

---
+++ SampleApp/SampleApp/Navigation/TabBarRoutes.swift
@@ -0,0 +1,12 @@
+//
+//  TabBarRoutes.swift
+//  SampleApp
+//
+//  Created by Themis Makedas on 18/1/24.
+//
+
+import Foundation
+
+public class TabBarRoutes {
+    var homeRoutingControllers: 
+}
