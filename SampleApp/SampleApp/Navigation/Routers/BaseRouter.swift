3cce914d9e55ddb76d95eb4b94cf7d8efa737460
Themis <themis017@windowslive.com>
2024-04-06 10:54:25 +0300

    When changing selected tab the also insert the rest view controllers in navigation controller before the main screen

--- SampleApp/SampleApp/Navigation/Routers/BaseRouter.swift
+++ SampleApp/SampleApp/Navigation/Routers/BaseRouter.swift
@@ -98,6 +98,23 @@ open class BaseRouter {
                 self.navigationController.viewControllers.insert(routingViewController, at: viewControllersCount)
             }
         }
+        
+        insertOtherTabBarCategoriesControllers(of: selectedTab)
+    }
+    
+    private func insertOtherTabBarCategoriesControllers(of selectedTab: TabBarCategory) {
+        
+        for tabBarCategory in TabBarCategory.allCases {
+            
+            if tabBarCategory != selectedTab,
+               let categoryRoutingViewControllers = TabBarRoutes.shared.tabBarRoutingControllers[tabBarCategory],
+               !categoryRoutingViewControllers.isEmpty {
+                
+                for categoryRoutingViewController in categoryRoutingViewControllers {
+                    self.navigationController.viewControllers.insert(categoryRoutingViewController, at: 0)
+                }
+            }
+        }
     }
     
     private func removeViewControllers(for selectedTab: TabBarCategory) {
@@ -121,189 +138,3 @@ open class BaseRouter {
         TabBarRoutes.shared.tabBarRoutingControllers[tabBarCategory] = currentRoutingViewControllers
     }
 }
-
-
-//open class BaseRouter {
-//    
-//    var navigationController: UINavigationController
-//    
-//    init(navigationController: UINavigationController) {
-//        self.navigationController = navigationController
-//    }
-//    
-//    @MainActor
-//    public func popScene() {
-//        if let routingController = navigationController.topViewController as? RoutingUIHostingController<AnyView>,
-//           !routingController.isRoot {
-//            switch routingController.tabCategory {
-//            case .home:
-//                if TabBarRoutes.shared.homeRoutingControllers.count > 0 {
-//                    TabBarRoutes.shared.homeRoutingControllers = TabBarRoutes.shared.homeRoutingControllers.dropLast()
-//                }
-//                
-//            case .search:
-//                if TabBarRoutes.shared.searchRoutingControllers.count > 0 {
-//                    TabBarRoutes.shared.searchRoutingControllers = TabBarRoutes.shared.searchRoutingControllers.dropLast()
-//                }
-//            case .notifications:
-//                if TabBarRoutes.shared.notificationsRoutingControllers.count > 0 {
-//                    TabBarRoutes.shared.notificationsRoutingControllers = TabBarRoutes.shared.notificationsRoutingControllers.dropLast()
-//                }
-//            case .profile:
-//                if TabBarRoutes.shared.profileRoutingControllers.count > 0 {
-//                    TabBarRoutes.shared.profileRoutingControllers = TabBarRoutes.shared.profileRoutingControllers.dropLast()
-//                }
-//            case .none:
-//                break
-//            }
-//            
-//            DispatchQueue.main.async {
-//                self.navigationController.popViewController(animated: true)
-//            }
-//        }
-//    }
-//    
-//    @MainActor
-//    public func dismissToRoot(for selectedTab: TabBarCategory) {
-//          
-//        switch selectedTab {
-//        case .home:
-//            TabBarRoutes.shared.homeRoutingControllers = []
-//            removeViewControllers(for: .home)
-//        case .search:
-//            TabBarRoutes.shared.searchRoutingControllers = []
-//            removeViewControllers(for: .search)
-//        case .notifications:
-//            TabBarRoutes.shared.notificationsRoutingControllers = []
-//            removeViewControllers(for: .notifications)
-//        case .profile:
-//            TabBarRoutes.shared.profileRoutingControllers = []
-//            removeViewControllers(for: .profile)
-//        }
-//    }
-//    
-//    @MainActor
-//    public func showPath(for selectedTab: TabBarCategory) {
-//        switch selectedTab {
-//        case .home:
-//            let homeViewControllers = TabBarRoutes.shared.homeRoutingControllers
-//            self.navigationController.viewControllers.removeAll { uiViewController in
-//                guard let routingController = uiViewController as? RoutingUIHostingController<AnyView>,
-//                      routingController.isRoot else {
-//                    return true
-//                }
-//                
-//                return false
-//            }
-//                        
-//            let viewControllers = homeViewControllers.reversed()
-//            var viewControllersCount = viewControllers.count
-//            
-//            for homeViewController in viewControllers {
-//                if viewControllersCount == homeViewControllers.count {
-//                    self.navigationController.pushViewController(homeViewController, animated: false)
-//                    viewControllersCount = viewControllersCount - 1
-//                    continue
-//                }
-//                
-//                if viewControllersCount == 0 {
-//                    break
-//                } else {
-//                    self.navigationController.viewControllers.insert(homeViewController, at: viewControllersCount)
-//                }
-//            }
-//        case .search:
-//            let searchViewControllers = TabBarRoutes.shared.searchRoutingControllers
-//            self.navigationController.viewControllers.removeAll { uiViewController in
-//                guard let routingController = uiViewController as? RoutingUIHostingController<AnyView>,
-//                      routingController.isRoot else {
-//                    return true
-//                }
-//                
-//                return false
-//            }
-//                        
-//            let viewControllers = searchViewControllers.reversed()
-//            var viewControllersCount = viewControllers.count
-//            
-//            for searchViewController in viewControllers {
-//                if viewControllersCount == searchViewControllers.count {
-//                    self.navigationController.pushViewController(searchViewController, animated: false)
-//                    viewControllersCount = viewControllersCount - 1
-//                    continue
-//                }
-//                
-//                if viewControllersCount == 0 {
-//                    break
-//                } else {
-//                    self.navigationController.viewControllers.insert(searchViewController, at: viewControllersCount)
-//                }
-//            }
-//        case .notifications:
-//            let notificationsViewControllers = TabBarRoutes.shared.notificationsRoutingControllers
-//            self.navigationController.viewControllers.removeAll { uiViewController in
-//                guard let routingController = uiViewController as? RoutingUIHostingController<AnyView>,
-//                      routingController.isRoot else {
-//                    return true
-//                }
-//                
-//                return false
-//            }
-//                        
-//            let viewControllers = notificationsViewControllers.reversed()
-//            var viewControllersCount = viewControllers.count
-//            
-//            for notificationViewController in viewControllers {
-//                if viewControllersCount == notificationsViewControllers.count {
-//                    self.navigationController.pushViewController(notificationViewController, animated: false)
-//                    viewControllersCount = viewControllersCount - 1
-//                    continue
-//                }
-//                
-//                if viewControllersCount == 0 {
-//                    break
-//                } else {
-//                    self.navigationController.viewControllers.insert(notificationViewController, at: viewControllersCount)
-//                }
-//            }
-//        case .profile:
-//            let profileViewControllers = TabBarRoutes.shared.profileRoutingControllers
-//            self.navigationController.viewControllers.removeAll { uiViewController in
-//                guard let routingController = uiViewController as? RoutingUIHostingController<AnyView>,
-//                      routingController.isRoot else {
-//                    return true
-//                }
-//                
-//                return false
-//            }
-//                        
-//            let viewControllers = profileViewControllers.reversed()
-//            var viewControllersCount = viewControllers.count
-//            
-//            for profileViewController in viewControllers {
-//                if viewControllersCount == profileViewControllers.count {
-//                    self.navigationController.pushViewController(profileViewController, animated: false)
-//                    viewControllersCount = viewControllersCount - 1
-//                    continue
-//                }
-//                
-//                if viewControllersCount == 0 {
-//                    break
-//                } else {
-//                    self.navigationController.viewControllers.insert(profileViewController, at: viewControllersCount)
-//                }
-//            }
-//        }
-//    }
-//    
-//    private func removeViewControllers(for selectedTab: TabBarCategory) {
-//        self.navigationController.viewControllers.removeAll { viewController in
-//            guard let routingController = viewController as? RoutingUIHostingController<AnyView>,
-//                  routingController.tabCategory == selectedTab  else {
-//                return false
-//            }
-//            
-//            return true
-//        }
-//    }
-//}
